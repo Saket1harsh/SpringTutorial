f78eef853bfffe5ac459058abedd9b1d23b3d06d
Saket Kumar <Saket.Kumar@godigit.com>
2024-08-22 12:36:46 +0530

    [Saket] add : 3rd Program of Day23

--- Day23/Day23/src/main/java/com/example/Day23/service/EmployeeImplementation.java
+++ Day23/Day23/src/main/java/com/example/Day23/service/EmployeeImplementation.java
@@ -4,13 +4,50 @@ import com.example.Day23.entity.Employee;
 import com.example.Day23.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.List;
+import java.util.Objects;
+
 public class EmployeeImplementation implements EmployeeService{
 
     @Autowired
     private EmployeeRepository employeeRepository;
 
     @Override
-    public Employee saveEmployee(Employee employee) {
+    public Employee createEmployee(Employee employee) {
         return (Employee) employeeRepository.save(employee);
+    }
+
+    @Override
+    public Employee getEmployeeById(int id) {
+        return employeeRepository.findById((long) id).get();
+    }
+
+    @Override
+    public List<Employee> getAllEmployee() {
+        return employeeRepository.findAll();
+    }
+
+    @Override
+    public Employee updateEmployee(int id, Employee employee) {
+        Employee emp = employeeRepository.findById((long) id).get();
+
+        if(Objects.nonNull(employee.getFirstName())&&
+                !"".equalsIgnoreCase(employee.getFirstName())){
+            emp.setFirstName(employee.getFirstName());
+        }
+        if(Objects.nonNull(employee.getLastName())&&
+                !"".equalsIgnoreCase(employee.getLastName())){
+            emp.setLastName(employee.getLastName());
+        }
+        if(Objects.nonNull(employee.getDepartment())&&
+                !"".equalsIgnoreCase(employee.getDepartment())){
+            emp.setDepartment(employee.getDepartment());
+        }
+        return employeeRepository.save(emp);
+    }
+
+    @Override
+    public void deleteEmployee(long id) {
+        employeeRepository.deleteById(id);
     }
 }
--- Day23/Day23/src/main/java/com/example/Day23/service/EmployeeService.java
+++ Day23/Day23/src/main/java/com/example/Day23/service/EmployeeService.java
@@ -1,7 +1,15 @@
 package com.example.Day23.service;
 
 import com.example.Day23.entity.Employee;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public interface EmployeeService {
-    public Employee saveEmployee(Employee employee);
+    public Employee createEmployee(Employee employee);
+    public Employee getEmployeeById(int id);
+    public List<Employee> getAllEmployee();
+    public Employee updateEmployee(int id,Employee employee);
+    public void deleteEmployee(long id);
 }
